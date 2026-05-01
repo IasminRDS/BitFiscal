b0bbce6ceb72cc619ef3505d49300bba07d07914
Iasmin Ribeiro de Souza <iasminrds1@gmail.com>
2026-04-30 21:20:35 -0300

    Arrumando o código

--- repo_reader.py
+++ repo_reader.py
@@ -3,24 +3,24 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# 1. Carregar arquivos
+#Carregar arquivos
 loader = DirectoryLoader(".", glob="**/*.py", loader_cls=TextLoader)
 docs = loader.load()
 
-# 2. Dividir
+#Dividir
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = splitter.split_documents(docs)
 
-# 3. Criar embeddings
+#Criar embeddings
 embeddings = OpenAIEmbeddings()
 
-# 4. Criar banco vetorial
+#Criar banco vetorial
 db = FAISS.from_documents(chunks, embeddings)
 
-# 5. Modelo
+#Modelo
 llm = ChatOpenAI(model="gpt-4o-mini")
 
-# 6. Pergunta
+#Pergunta
 query = "O que esse projeto faz?"
 
 docs_encontrados = db.similarity_search(query)
